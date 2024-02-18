55633eaefb1af6223ec55941bdd5456e3dd4d608
Olivier van Helden <magic@miley.magiiic.com>
2024-02-19 00:33:25 +0100

    fix basic locales

--- includes/translations.php
+++ includes/translations.php
@@ -4,7 +4,7 @@ $language = empty($language) ? 'en_US' : $language;
 putenv("LANG=".$language); 
 setlocale(LC_ALL, $language);
 
-$domain = "openkj";
-bindtextdomain($domain, BASE_DIR . "/locale");
+$domain = "messages";
+bindtextdomain($domain, BASE_DIR . "/locales");
 bind_textdomain_codeset($domain, 'UTF-8');
 textdomain($domain);
