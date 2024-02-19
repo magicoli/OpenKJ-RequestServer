28478d77b5c7cc7521f646edaed18b71cce865a2
Olivier van Helden <magic@miley.magiiic.com>
2024-02-19 01:58:06 +0100

    honor user language preference

--- includes/translations.php
+++ includes/translations.php
@@ -1,6 +1,15 @@
 <?php
 
-$language = empty($language) ? 'en_US' : $language;
+$language = isset($language) ? $language : 'en_US'; // Default to 'en_US' if $language is not set
+// If the user has set a language preference, use that
+if (isset($_SERVER['HTTP_ACCEPT_LANGUAGE'])) {
+    // Extract the language and country part
+    preg_match('/^([a-z]{2}_[A-Z]{2})/', str_replace('-', '_', $_SERVER['HTTP_ACCEPT_LANGUAGE']), $matches);
+    if ($matches) {
+        $language = $matches[1];
+    }
+}
+
 putenv("LANG=".$language); 
 setlocale(LC_ALL, $language);
 
